f60e3ae44d75b0f837bbd376cac2af00dc003b07
Phi Long <91886384+PhiLongVN@users.noreply.github.com>
2022-04-22 17:35:39 +0700

    uplaod2

--- script/script.js
+++ script/script.js
@@ -8,23 +8,71 @@ function handleClick() {
 
 /* -------------------------------- check ------------------------------- */
 
-handleClick();
-/* --- SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS -- */
+// handleClick();
 
+/* ============================================ */
+/*                 TOGGLE THEME                 */
+/* ============================================ */
 const theme = document.querySelector('.theme');
-//lấy thằng input toggle để check
-const checkBox = document.querySelector("");
+const colorBody = document.querySelector('body');
 
-checkBox.onchange = () => {
-  document.body.classList.toggle("active");
-};
+theme.addEventListener('click', handleTheme);
 
-// CSS THÊM FILTER VÀO BODY
-// body.active {
-//   filter: invert(1) hue-rotate(180deg);
-// }
+function handleTheme() {
+  if (colorBody.classList.contains('light')) {
+    colorBody.className = 'dark';
+    theme.innerHTML = `<ion-icon class="icon" name="moon"></ion-icon>
+                       <span>Dark Mode</span>`;
+  } else {
+    colorBody.className = 'light';
+    theme.innerHTML = `<ion-icon class="icon" name="sunny"></ion-icon>
+                       <span>Light Mode</span>`;
+  }
+}
 
-// muốn giữ lại màu cho img
-// body.active img {
-//   filter: invert(1) hue-rotate(180deg);
-// }
+/* ============================================ */
+/*                  RENDER FLAG                 */
+/* ============================================ */
+const mainShow = document.querySelector('.main-show');
+const linkjob = 'https://restcountries.com/v3.1/all';
+async function fetchjob() {
+  const res = await fetch(linkjob);
+  const data = await res.json();
+
+  return data;
+}
+
+function createBlock(data) {
+  return (block = `<div class="country-block">
+  <img src="${data.coatOfArms.png}" alt="" />
+  <div class="country-detail">
+    <span class="name">${data.name.common}</span>
+    <div class="geo">
+      <span
+        >Population:
+        <div>${data.population}</div>
+      </span>
+      <span>
+        Region:
+        <div>${data.region}</div>
+      </span>
+      <span>
+        Capital:
+        <div>${data.capital}</div>
+      </span>
+    </div>
+  </div>
+</div>`);
+}
+
+function renderBlock() {
+  let listFlag = [];
+  fetchjob().then((data) => {
+    data.forEach((country) => {
+      listFlag += createBlock(country);
+      mainShow.innerHTML = listFlag;
+    });
+  });
+}
+
+renderBlock();
