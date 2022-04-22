cb34fc159a4c23c8d1d895433c894b6ffc474ab6
Phi Long <91886384+PhiLongVN@users.noreply.github.com>
2022-04-23 00:53:28 +0700

    ipload

--- script/script.js
+++ script/script.js
@@ -34,17 +34,17 @@ function handleTheme() {
 /*                  RENDER FLAG                 */
 /* ============================================ */
 const mainShow = document.querySelector('.main-show');
-const linkjob = 'https://restcountries.com/v3.1/all';
-async function fetchjob() {
-  const res = await fetch(linkjob);
+const linkJob = 'https://restcountries.com/v3.1/all';
+async function fetchJob() {
+  const res = await fetch(linkJob);
   const data = await res.json();
 
   return data;
 }
 
 function createBlock(data) {
-  return (block = `<div class="country-block">
-  <img src="${data.coatOfArms.png}" alt="" />
+  return (block = `<div data-name="${data.name.common}" class="country-block">
+  <img src="${data.flags.png}" alt="" />
   <div class="country-detail">
     <span class="name">${data.name.common}</span>
     <div class="geo">
@@ -67,12 +67,138 @@ function createBlock(data) {
 
 function renderBlock() {
   let listFlag = [];
-  fetchjob().then((data) => {
+  fetchJob().then((data) => {
+  console.log("renderBlock -> data", data)
     data.forEach((country) => {
       listFlag += createBlock(country);
       mainShow.innerHTML = listFlag;
+      clickBLock();
     });
   });
 }
 
 renderBlock();
+
+/* ============================================ */
+/*                    SEARCH                    */
+/* ============================================ */
+
+const searchText = document.querySelector('.search-text input');
+
+searchText.addEventListener('change', handleSearch);
+
+function handleSearch() {
+  let listFlag = [];
+  let a = searchText.value.toLowerCase();
+
+  fetchJob().then((data) => {
+    data.forEach((country) => {
+      if (country.name.common.toLowerCase().includes(a)) {
+        listFlag += createBlock(country);
+        mainShow.innerHTML = listFlag;
+      }
+    });
+  });
+}
+
+/* ============================================ */
+/*                 SEARCH REGION                */
+/* ============================================ */
+const searchRegion = document.querySelector('.search-region select');
+
+searchRegion.addEventListener('change', handleSearchRegion);
+
+function handleSearchRegion() {
+  let listFlag = [];
+  let a = searchRegion.value.toLowerCase();
+
+  fetchJob().then((data) => {
+    data.forEach((country) => {
+      if (country.region.toLowerCase().includes(a)) {
+        listFlag += createBlock(country);
+        mainShow.innerHTML = listFlag;
+      }
+    });
+  });
+}
+
+/* ============================================ */
+/*                  CLICK FLAG                  */
+/* ============================================ */
+
+function clickBLock() {
+  const flagBlock = document.querySelectorAll('.country-block');
+  flagBlock.forEach((flag) => {
+    flag.addEventListener('click', () => {
+      let a = flag.dataset.name;
+      fetchJob().then((data) => {
+        data.forEach((country) => {
+          if (country.name.common == a) {
+            console.log(country.borders);
+          }
+        });
+      });
+    });
+  });
+}
+
+function createDetail(data) {
+  return `  <div class="img-flag">
+  <img src="${data.flags.png}" alt="" />
+</div>
+
+<div class="detail-flag">
+  <h2>${data.name.common}</h2>
+
+  <div class="geo-country">
+    <div class="geo geo1">
+      <span
+        >Native Name:
+        <div>aaaaaaaaaaaaa</div></span
+      >
+      <span
+        >Population:
+        <div>aaaaaaaaaaaaa</div></span
+      >
+      <span
+        >Region:
+        <div>aaaaaaaaaaaaa</div></span
+      >
+      <span
+        >Sub Region:
+        <div>aaaaaaaaaaaaa</div></span
+      >
+      <span
+        >Capital:
+        <div>aaaaaaaaaaaaa</div></span
+      >
+    </div>
+
+    <div class="geo geo2">
+      <span
+        >Top Level Domain:
+        <div>aaaaaaaaaaaaa</div></span
+      >
+      <span
+        >Currencies:
+        <div>aaaaaaaaaaaaa</div></span
+      >
+      <span
+        >Languages:
+        <div>aaaaaaaaaaaaa</div></span
+      >
+    </div>
+  </div>
+
+  <div class="border-country">
+    <span class="border"> Border Countries </span>
+    <div class="borderblock">
+     ${createBorder(data.border)}
+    </div>
+  </div>
+</div>`;
+}
+
+function createBorder(data) {
+  console.log(data);
+}
